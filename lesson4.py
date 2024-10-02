693643588f639129e05bf965f01ba6e74239e269
dani-0001 <godsownayembe@gmail.com>
2024-10-02 12:22:03 +0100

    first commit

---
+++ lesson4.py
@@ -0,0 +1,9 @@
+def daniel_ayembe_func():
+    name = "Daniel"
+    height = 6.7
+    age = 40
+    is_male = True 
+    print(name, height, age, is_male)
+ 
+ #this is my first function 
+daniel_ayembe_func()
